639163db6f257b063f8de9119412c083c9986d96
MeiVinEight <MeiVinEight@outlook.com>
2026-03-18 10:51:25 +0800

    🚀CORE: First Version 0.0.1, base boot

--- CORE/src/main.c
+++ CORE/src/main.c
@@ -1,3 +1,4 @@
+#include <core.h>
 #include <intrinsic.h>
 #include <console.h>
 #include <interrupt/interrupt.h>
@@ -6,56 +7,53 @@
 #include <arch/processor.h>
 #include <interrupt/apic.h>
 #include <timer/timer.h>
+#include <acpi/acpi.h>
+#include <driver/pci/pci.h>
 
-#include "driver/pci/pci.h"
+QWORD __stdcall coreCRTStartup();
 
-COREAPI SUPERNOVA_SYSTEM_TABLE *SYSTEM_TABLE = (SUPERNOVA_SYSTEM_TABLE *) 0x0040;
+COREAPI SUPERNOVA_SYSTEM_TABLE *SYSTEM_TABLE = (SUPERNOVA_SYSTEM_TABLE *) SYSTEM_ADDRESS;
 
-
-unsigned long long _DllMainCRTStartup()
+void kprint_cpu()
 {
-	setup_paging();
-
-	QWORD buf[2] = { 0, 0 };
-	__memset128((void *) SYSTEM_TABLE->FBB, buf, (SYSTEM_TABLE->PPL * SYSTEM_TABLE->VRES * 4) / 16);
-	simple_output("Supernova OS\n");
-
-	setup_processor();
-	setup_segment();
-	setup_apic();
-	setup_interrupe();
-
-	DWORD cpuid[4];
 	char brand[51];
-	brand[48] = '\r';
-	brand[49] = '\n';
-	brand[50] = 0;
-	DWORD *brandx = (DWORD *) brand;
-	__cpuid((int *) cpuid, (int) 0x80000002);
-	brandx[0] = cpuid[0];
-	brandx[1] = cpuid[1];
-	brandx[2] = cpuid[2];
-	brandx[3] = cpuid[3];
-	__cpuid((int *) cpuid, (int) 0x80000003);
-	brandx[4] = cpuid[0];
-	brandx[5] = cpuid[1];
-	brandx[6] = cpuid[2];
-	brandx[7] = cpuid[3];
-	__cpuid((int *) cpuid, (int) 0x80000004);
-	brandx[8] = cpuid[0];
-	brandx[9] = cpuid[1];
-	brandx[10] = cpuid[2];
-	brandx[11] = cpuid[3];
+	cpuid_brand(brand);
+	brand[48] = 0;
 	simple_output("CPU #");
-	simple_output_number(apic_current_id());
+	simple_output_number(cpu_local_apic_id());
 	outchar(' ');
 	simple_output(brand);
 	outchar('\n');
+}
+QWORD core_mapping(QWORD addr)
+{
+	return addr | SYSTEM_ADDRESS;
+}
+unsigned long long _DllMainCRTStartup()
+{
+	setup_paging();
+	setup_segment();
+	return ((QWORD (*)()) core_mapping((QWORD) coreCRTStartup))();
+}
+QWORD coreCRTStartup()
+{
+	setup_interrupe();
+	setup_page_fault();
+	setup_console();
 
+	simple_output("Supernova OS\n");
+
+	setup_processor();
+	setup_apic();
+
+	kprint_cpu();
+
+	setup_acpi();
 	setup_timer();
+	apic_setup_multiprocessor();
 	setup_pci();
 
-
+	/*
 	MEMORY_REGION *beg = SYSTEM_TABLE->MEMORY;
 	//             0000000000000000 | 0000000000000000 | 00000000
 	simple_output("Base Address       Length             Type\r\n");
@@ -69,14 +67,12 @@ unsigned long long _DllMainCRTStartup()
 		simple_output("\r\n");
 		beg++;
 	}
-
-	simple_output("GDT: ");
-	simple_output_address((QWORD) SYSTEM_TABLE->GDT, 16);
-	simple_output("\n");
+	*/
 
 	simple_output("OK\n");
 
 	*((DWORD *) 0x100000000ULL) = 1;
 
 	while (1) __halt();
+	return 0;
 }

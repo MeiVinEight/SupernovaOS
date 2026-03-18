681eb552d2d15f27e84302cd40f2675a400eaa76
MeiVinEight <MeiVinEight@outlook.com>
2026-03-18 10:35:34 +0800

    ⭐CORE: simple ACPI structs

--- CORE/src/acpi/acpi.h
+++ CORE/src/acpi/acpi.h
@@ -5,4 +5,59 @@
 #ifndef SUPERNOVA_ACPI_H
 #define SUPERNOVA_ACPI_H
 
+#include  <types.h>
+
+#define ACPI_SIGNATURE_MADT 0x43495041 // APIC
+
+typedef struct _ACPI_RSDP
+{
+	char SIGN[8];
+	BYTE CHKS;
+	char OEMS[6];
+	BYTE REVI;
+	DWORD RSDT;
+	// ACPI 2.0
+	DWORD LENH;
+	QWORD XSDT;
+	BYTE XCHK;
+	BYTE RSV0[3];
+} ACPI_RSDP;
+#pragma pack(push, 4)
+typedef struct _ACPI_ADDRESS
+{
+	BYTE  SPID; // Space ID, 0: System Memory, 1: System I/O
+	BYTE  WIDT; // Register bit width
+	BYTE  OFST; // Register bit offset
+	BYTE  RSV0;
+	QWORD ADDR;
+} ACPI_ADDRESS;
+#pragma pack(pop)
+typedef struct _ACPI_SDT_HEADER
+{
+	BYTE SIGN[4];
+	DWORD LENG;
+	BYTE RVSN;
+	BYTE CSUM;
+	BYTE OEMD[6];
+	BYTE OEMT[8];
+	DWORD OEMR;
+	DWORD CRTI;
+	DWORD CRTR;
+} ACPI_SDT_HEADER;
+typedef struct _ACPI_RSDT
+{
+	ACPI_SDT_HEADER HEAD;
+	DWORD TABLE[];
+} ACPI_RSDT;
+#pragma pack(push, 4)
+typedef struct _ACPI_XSDT
+{
+	ACPI_SDT_HEADER HEAD;
+	QWORD TABLE[];
+} ACPI_XSDT;
+#pragma pack(pop)
+
+void setup_acpi();
+ACPI_RSDP *acpi_find_rsdp();
+
 #endif //SUPERNOVA_ACPI_H

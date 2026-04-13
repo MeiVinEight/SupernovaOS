21cd64f11c0768c04d07a89636039d9b190cdbf1
MeiVinEight <MeiVinEight@outlook.com>
2026-04-13 15:43:12 +0800

    🎨CORE: remove memset for tss

--- CORE/src/arch/tss.c
+++ CORE/src/arch/tss.c
@@ -1,12 +1,9 @@
 #include <arch/tss.h>
 #include <intrinsic.h>
-#include <stdio.h>
 
 void load_tss(void *tss0, void *gdt0, WORD off)
 {
-	TASK_STATE_SEGMENT *tss = tss0;
-	QWORD tssAddr = (QWORD) tss;
-	__memset(tss, 0, sizeof(TASK_STATE_SEGMENT));
+	QWORD tssAddr = (QWORD) tss0;
 	TSS_DESCRIPTOR *tssd = (TSS_DESCRIPTOR *) (((QWORD) gdt0) + off);
 	tssd->LIMT = 0x67; // 103, 1 less than sizeof (tss)
 	tssd->ADD0 = (tssAddr >>  0) & 0xFFFF;

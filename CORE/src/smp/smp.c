a96e84600ae802b68f9fc68e71bfadea422b8f2e
MeiVinEight <MeiVinEight@outlook.com>
2026-04-20 17:06:16 +0800

    ⚠️CORE: IPI should set EOI

--- CORE/src/smp/smp.c
+++ CORE/src/smp/smp.c
@@ -5,5 +5,5 @@
 void int_smp(INTERRUPT_STACK *stack)
 {
 	printf("CPU #%lu INT: %02llX @ RIP %p: Multiprocessor\n", apic_current_id(), stack->INT, (void *) stack->RIP);
-	//eoi_apic(0);
+	eoi_apic(0);
 }
